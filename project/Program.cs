2775f7689cdb9e6f3a0bac14f8f161ec96424ebc
Yaohan <yaohan.zais@mondo.com.au>
2021-02-25 09:49:03 +1100

    Initial commit

---
+++ project/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
